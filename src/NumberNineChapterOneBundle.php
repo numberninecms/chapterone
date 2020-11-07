81f578b1a29ef5e77ac8c937124df4dc1c09adc0
William Arin <williamarin.dev@gmail.com>
2020-11-08 00:00:12 +0800

    fix: stop using a custom bundle class to make flex auto-recipe work

--- src/NumberNineChapterOneBundle.php
+++ src/NumberNineChapterOneBundle.php
@@ -10,10 +10,13 @@
 
 namespace NumberNine\ChapterOne;
 
-use NumberNine\Common\Bundle\Bundle;
+use NumberNine\Common\Bundle\BundleTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class NumberNineChapterOneBundle extends Bundle
 {
+    use BundleTrait;
+
     protected function getAlias(): string
     {
         return 'numbernine_chapterone';
