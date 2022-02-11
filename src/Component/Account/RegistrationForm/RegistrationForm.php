7d900c9c41d792b7420d23f541f53d87dfa36aa3
William Arin <williamarin.dev@gmail.com>
2022-02-11 12:17:27 +0800

    feat: upgrade for cms 0.4

--- src/Component/Account/RegistrationForm/RegistrationForm.php
+++ src/Component/Account/RegistrationForm/RegistrationForm.php
@@ -15,111 +15,57 @@ use NumberNine\Entity\User;
 use NumberNine\Event\RegistrationFormBuilderEvent;
 use NumberNine\Event\RegistrationFormSuccessEvent;
 use NumberNine\Form\User\RegistrationFormType;
-use NumberNine\Model\Component\ComponentInterface;
+use NumberNine\Model\Component\AbstractFormComponent;
 use NumberNine\Security\UserAuthenticator;
 use NumberNine\Security\UserFactory;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormView;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Event\ControllerEvent;
-use Symfony\Component\HttpKernel\Event\ResponseEvent;
 
-class RegistrationForm implements ComponentInterface, EventSubscriberInterface
+class RegistrationForm extends AbstractFormComponent
 {
-    private FormFactoryInterface $formFactory;
-    private ?Request $request;
-    private UserFactory $userFactory;
-    private UserAuthenticator $userAuthenticator;
-    private EventDispatcherInterface $eventDispatcher;
-    private ?Response $response = null;
-
-    public static function getSubscribedEvents(): array
-    {
-        return [
-            ControllerEvent::class => 'processForm',
-            ResponseEvent::class => 'alterResponse',
-        ];
-    }
+    protected array $supportedTemplates = ['@ChapterOne/page/my_account.html.twig'];
 
     public function __construct(
-        FormFactoryInterface $formFactory,
-        RequestStack $requestStack,
-        UserFactory $userFactory,
-        UserAuthenticator $userAuthenticator,
-        EventDispatcherInterface $eventDispatcher
+        private FormFactoryInterface $formFactory,
+        private UserFactory $userFactory,
+        private UserAuthenticator $userAuthenticator,
+        private EventDispatcherInterface $eventDispatcher,
     ) {
-        $this->formFactory = $formFactory;
-        $this->userFactory = $userFactory;
-        $this->userAuthenticator = $userAuthenticator;
-        $this->eventDispatcher = $eventDispatcher;
-        $this->request = $requestStack->getCurrentRequest();
+        parent::__construct($formFactory, $eventDispatcher);
     }
 
-    private function getForm(): ?FormView
+    protected function getForm(): FormInterface
     {
-        if (!$this->request || !($form = $this->getFormObject())) {
-            return null;
+        if (!$this->form) {
+            $builder = $this->formFactory->createBuilder(RegistrationFormType::class, new User());
+
+            /** @var RegistrationFormBuilderEvent $event */
+            $event = $this->eventDispatcher->dispatch(new RegistrationFormBuilderEvent($builder));
+
+            $this->form = $event->getBuilder()->getForm();
         }
 
-        $form->handleRequest($this->request);
-
-        return $form->createView();
+        return $this->form;
     }
 
-    private function getFormObject(): ?FormInterface
+    protected function handleSubmittedAndValidForm(Request $request): Response
     {
-        if (!$this->request) {
-            return null;
-        }
+        $form = $this->getForm();
 
-        $builder = $this->formFactory->createBuilder(RegistrationFormType::class, new User());
+        $user = $this->userFactory->createUser(
+            $form->get('username')->getData(),
+            $form->get('email')->getData(),
+            $form->get('plainPassword')->getData(),
+        );
 
-        /** @var RegistrationFormBuilderEvent $event */
-        $event = $this->eventDispatcher->dispatch(new RegistrationFormBuilderEvent($builder));
+        $response = $this->userAuthenticator->authenticateUser($user);
 
-        return $event->getBuilder()->getForm();
-    }
+        /** @var RegistrationFormSuccessEvent $registrationFormSuccessEvent */
+        $registrationFormSuccessEvent = $this->eventDispatcher->dispatch(new RegistrationFormSuccessEvent($user, $response));
 
-    public function processForm(ControllerEvent $event): void
-    {
-        if (!($form = $this->getFormObject())) {
-            return;
-        }
-
-        $form->handleRequest($event->getRequest());
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $user = $this->userFactory->createUser(
-                $form->get('username')->getData(),
-                $form->get('email')->getData(),
-                $form->get('plainPassword')->getData()
-            );
-
-            $response = $this->userAuthenticator->authenticateUser($user);
-
-            /** @var RegistrationFormSuccessEvent $event */
-            $event = $this->eventDispatcher->dispatch(new RegistrationFormSuccessEvent($user, $response));
-
-            $this->response = $event->getResponse();
-        }
-    }
-
-    public function alterResponse(ResponseEvent $event): void
-    {
-        if ($this->response) {
-            $event->setResponse($this->response);
-        }
-    }
-
-    public function getTemplateParameters(): array
-    {
-        return [
-            'form' => $this->getForm(),
-        ];
+        return $registrationFormSuccessEvent->getResponse();
     }
 }
